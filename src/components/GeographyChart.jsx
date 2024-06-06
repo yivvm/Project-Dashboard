1f459371199373c9395b2ffb78c56a8fcd3e9269
Yiwen Zhang <yiwenmary@gmail.com>
2024-06-06 10:19:13 -0400

    create Progress Circle

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -4,7 +4,7 @@ import { tokens } from "../theme"
 import { mockGeographyData as data } from "../data/mockData"
 import { geoFeatures } from "../data/mockGeoFeatures"
 
-export default function GeographyChart({ isDashboard }) {
+export default function GeographyChart({ isDashboard = false }) {
     const theme = useTheme()
     const colors = tokens(theme.palette.mode)
 
--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,5 +1,4 @@
-import { Box, colors } from '@mui/material'
-import { useTheme } from '@mui/material'
+import { Box, useTheme  } from '@mui/material'
 import { tokens } from '../../theme'
 import Header from '../../components/Header'
 import GeographyChart from '../../components/GeographyChart'
@@ -7,11 +6,11 @@ import GeographyChart from '../../components/GeographyChart'
 export default function Geography() {
     const theme = useTheme()
     const colors = tokens(theme.palette.mode)
-    
+
   return (
     <Box m='20px'>
         <Header title='Geography Chart' subtitle='Simple Gepgraphy Chart' />
-        <Box height='75vh' border={`1px ${colors.grey[100]}`} borderRadius='4px'>
+        <Box height='75vh' border={`1px solid ${colors.grey[100]}`} borderRadius='4px'>
             <GeographyChart />
         </Box>
     </Box>
