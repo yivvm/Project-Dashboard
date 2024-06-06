bb5b023ef46ad16d0d6ceea9ea0e5172d82e954f
Yiwen Zhang <yiwenmary@gmail.com>
2024-06-05 22:51:38 -0400

    update PieChart component and Pie page

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -54,7 +54,7 @@ export default function PieChart() {
             ]
         }}
         arcLinkLabelsSkipAngle={10}
-        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsTextColor={colors.grey[100]}
         arcLinkLabelsThickness={2}
         arcLinkLabelsColor={{ from: 'color' }}
         enableArcLabels={false}
@@ -86,56 +86,6 @@ export default function PieChart() {
                 rotation: -45,
                 lineWidth: 6,
                 spacing: 10
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'ruby'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'c'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'go'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'python'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'scala'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'lisp'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'elixir'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'javascript'
-                },
-                id: 'lines'
             }
         ]}
         legends={[
